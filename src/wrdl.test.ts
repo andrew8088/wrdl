6bc2ee3be4188d148628d03931e37a41fa90e9c9
Andrew Burgess <andrew@burgess.io>
2022-02-21 21:14:12 -0500

    wip

--- src/wrdl.test.ts
+++ src/wrdl.test.ts
@@ -2,25 +2,25 @@ import * as Wrdl from './wrdl';
 
 describe('checkGuess', () => {
     it('identifies correct letters', () => {
-        expect(Wrdl.checkGuess("a", "a")).toEqual(["C"]);
+        expect(Wrdl.scoreGuess("a", "a")).toEqual(["C"]);
     });
 
     it("identifies incorrect letters", () => {
-        expect(Wrdl.checkGuess("a", "b")).toEqual(['I']);
+        expect(Wrdl.scoreGuess("a", "b")).toEqual(['I']);
     });
 
     it('identifies almost letters', () => {
-        expect(Wrdl.checkGuess("ab", "bc")).toEqual(['I', 'A']);
+        expect(Wrdl.scoreGuess("ab", "bc")).toEqual(['I', 'A']);
     });
 
     it('identifies a duplicate guess letter as wrong', () => {
-        expect(Wrdl.checkGuess("bob", "bco")).toEqual(['C', 'A', 'I']); 
+        expect(Wrdl.scoreGuess("bob", "bco")).toEqual(['C', 'A', 'I']); 
     });
 
     it('identifies a duplicate guess letter as almost', () => {
-        expect(Wrdl.checkGuess("bob", "bbo")).toEqual(['C', 'A', 'A']); 
+        expect(Wrdl.scoreGuess("bob", "bbo")).toEqual(['C', 'A', 'A']); 
     });
     it('identifies a duplicate guess letter as correct', () => {
-        expect(Wrdl.checkGuess("bob", "bob")).toEqual(['C', 'C', 'C']); 
+        expect(Wrdl.scoreGuess("bob", "bob")).toEqual(['C', 'C', 'C']); 
     });
 });
--- src/wrdl.ts
+++ src/wrdl.ts
@@ -1,18 +1,25 @@
 import words from './words';
 
 type LetterScore = "I" | "A" | "C";
-type WordScore = Array<LetterScore>;
+type WordScore = LetterScore[];
 
 const INCORRECT: LetterScore = "I";
 const ALMOST: LetterScore = "A";
 const CORRECT: LetterScore = "C";
+
+type Game = {
+    answer: string;
+    guessesRemaining: number;
+    guesses: string[];
+    scores: WordScore[];
+}
 
 const getRandomInt = (min: number, max: number) => 
     Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const getWord = () => words[getRandomInt(0, words.length - 1)];
 
-export const checkGuess = (guess: string, answer: string): WordScore => {
+export const scoreGuess = (guess: string, answer: string): WordScore => {
     const guessLetters = guess.split('');
     const answerLetters = answer.split('');
 
@@ -35,3 +42,22 @@ export const checkGuess = (guess: string, answer: string): WordScore => {
 
     return score;
 }
+
+export const createGame = (answer: string = getWord()): Game => {
+    return {
+        answer,
+        guessesRemaining: 6,
+        guesses: [],
+        scores: []
+    };
+}
+
+export const makeGuess(game: Game, guess: string): Game =>  {
+
+    return {
+        answer: game.answer,
+        guessesRemaining: game.guessesRemaining - 1,
+        guesses: [...game.guesses].concat([guess]),
+    }
+
+};
